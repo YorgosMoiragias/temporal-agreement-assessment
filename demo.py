631355b175753756170b9d23153fa504d3a83ed3
YorgosMoiragias <yorgos.moiragias@gmail.com>
2023-11-08 17:12:29 +0200

    Update

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import panel_evaluation
 
 # Define the csv file containing the temporal Envelopment ratings
-file_path="../Temporal Envelopment Data/Demo_Temporal_Envelopment_Assessment.csv"
+file_path="Temporal Envelopment Data/Demo_Temporal_Envelopment_Assessment.csv"
 
 # Find the assessors that are in agreement with the majority of the listening panel and those who are not
 AssessorToHold, AssessorsToBeRemoved=panel_evaluation.agreement_evaluation(file_path,True)
